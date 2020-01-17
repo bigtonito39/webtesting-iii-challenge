58992508b98c19fa4b39569693d0116e0cb3e86e
bigtonito39 <bigtonito@hotmail.com>
2020-01-16 22:54:40 -0500

    all test passing, just need couple of more in controls to meet mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from "react";
+import Controls from "./Controls";
+import {render, findAllByTestId} from  "@testing-library/react";
+
+describe("test from controls", ()=> {
+    test("is Controls rendering", () => {
+        render( <Controls />)
+    
+    })
+
+
+})
+
