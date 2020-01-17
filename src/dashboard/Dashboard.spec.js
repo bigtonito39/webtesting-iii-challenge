c6640ff7b7b3684e798752b7070bc5edd2bcf1eb
bigtonito39 <bigtonito@hotmail.com>
2020-01-16 19:34:31 -0500

    dependencies installed, yard installed it as well, first test pased

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+import {render} from "@testing-library/react";
+import Display from "../display/Display"
+
+it("are display and controls showing in the dashboard?", () => {
+ const wrapper = rtl.render(<Dashboard/>);
+ 
+})
