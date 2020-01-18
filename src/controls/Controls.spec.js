589dece6326b35a6d130e4063d18a2a308ec4c7b
bigtonito39 <bigtonito@hotmail.com>
2020-01-17 20:43:53 -0500

    mvp met

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,12 +1,31 @@
 // Test away!
 import React from "react";
 import Controls from "./Controls";
-import {render, findAllByTestId} from  "@testing-library/react";
+import {render, fireEvent} from  "@testing-library/react";
+
 
 describe("test from controls", ()=> {
+
+    //getByText is a testing library query
     test("is Controls rendering", () => {
         render( <Controls />)
     
+    })
+    test("if locked is true, gate will unlock", () =>{
+        const {getByText} = render(<Controls locked={true} />)
+        getByText(/Unlock Gate/i)
+    })
+
+    test("when clicked text buttons will event an action", () => {
+        const {getByText} = render(<Controls />)
+        const buttonIsClosed = getByText(/Close Gate/i);
+        const buttonIsLocked = getByText(/Lock Gate/i);
+        
+        fireEvent.click(buttonIsClosed);
+        getByText(/Close Gate/i);
+        fireEvent.click(buttonIsLocked);
+        getByText(/Lock Gate/i);
+
     })
 
 
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,7 +4,7 @@ import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Dashboard from "./Dashboard";
 import {render} from "@testing-library/react";
-import Display from "../display/Display"
+
 
 it("are display and controls showing in the dashboard?", () => {
  const wrapper = rtl.render(<Dashboard/>);
